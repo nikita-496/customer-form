24318713fa4b7ffbb8dde5eae2b8e2baae1a17ef
Nikita Shevkunov <shevkunovnikita97@gmail.com>
2021-12-28 21:18:55 +0500

    scss conversion

--- src/App.vue
+++ src/App.vue
@@ -118,8 +118,8 @@
 </script>
 
 <style lang="scss">
-  @import "./scss/main.scss";
-
+  /*@import "@/scss/style.scss";*/
+  @import "@/assets/css/style.scss";
   body {
     background-image: url("../public/img/cover2.jpg");
     background-position: center;
